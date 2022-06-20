f7ab628c6055ba06c1012fcf55df83333494adb2
Rudani-el <107594764+Rudani-el@users.noreply.github.com>
2022-06-20 09:46:22 -0500

    Update process.php
    linea 118 de false a enable TRUE.

--- process.php
+++ process.php
@@ -115,7 +115,7 @@ function request(
 		'Authorization:Bearer '.$_SESSION['autorizador']));
 	curl_setopt($ch, CURLOPT_POST, 1);
 	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);// this should be set to true in production
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);// this should be set to true in production
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	$responseData = curl_exec($ch);
 	if(curl_errno($ch)) {
